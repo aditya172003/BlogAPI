31e44e9764db55b85c652b126bcc505a8ced4c80
Aditya Mahajan <adityam172003@gmail.com>
2023-12-22 23:12:17 +0530

    Added throatlink

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
 const cookieparser = require('cookie-parser')
 const dotenv = require("dotenv").config();
+const {rateLimiterUsingThirdParty}= require('./middlewares/rateLimit');
 
 connectdb();
 
@@ -27,7 +28,7 @@ const port = process.env.PORT || 8080;
 // app.use(cors(corsOptions));
 app.use(cookieparser())
 app.use(express.json());
-
+app.use(rateLimiterUsingThirdParty);
 app.get("/",(req,res)=>{
   res.send("from express server ")
 
